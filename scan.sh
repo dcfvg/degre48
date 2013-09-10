db05be9280455fe7eab6beec5c5ea008ebf938da
dcfvg <qwsx@dcfvg.com>
2013-09-10 13:07:21 +0200

    better image conversion

--- scan.sh
+++ scan.sh
@@ -1,21 +1,26 @@
-while true; do	
-	now=$(date +"%y.%m.%d-%H.%M.%S")
-	
-	echo "\nscan EPSON @ $now"
-	scanimage --format tiff --mode Gray --resolution 100 -d epson2:libusb:001:067 -p > A-$now.tiff
-	
-	echo "\nscan HP @ $now"
-	scanimage --format tiff --mode Gray --resolution 100 -d hpaio:/usb/PSC_1600_series?serial=MY54PD10RQL0 -p > B-$now.tiff
-	
-	echo "\nscan HP @ $now"
-	scanimage --format tiff --mode Gray --resolution 100 -d epson2:libusb:001:068 -p > C-$now.tiff
-	
-  convert A-$now.tiff -format jpeg scans/A-$now.jpeg
-  convert B-$now.tiff -format jpeg scans/B-$now.jpeg
-  convert C-$now.tiff -format jpeg scans/C-$now.jpeg
+# set -x
+function allscan48 {
+  rawDir=$1   # raw directory
+  scaDir=$2   # scan directory for latex (jpeg)
+  pdfDir=$3   # pdf directory for print
   
-  rm A-$now.tiff
-  rm B-$now.tiff
-  rm C-$now.tiff
+  dpi=100
+  now=$(date +"%y.%m.%d-%H.%M.%S")
+ 	
+ 	echo "scan 01             "
+	scanimage --format tiff --mode Gray --resolution $dpi -d epson2:libusb:001:010 -p > $rawDir/01.tiff
+	
+	echo "scan 02             "
+	scanimage --format tiff --mode Gray --resolution $dpi -d hpaio:/usb/PSC_1600_series?serial=MY54PD10RQL0 -p > $rawDir/02.tiff
+	
+	echo "scan 03             "
+	scanimage --format tiff --mode Gray --resolution $dpi -d epson2:libusb:001:011 -p > $rawDir/03.tiff
+	
+	echo "$rawDir > jpeg > $scaDir"
+  mogrify -path $scaDir/ -format jpeg "$rawDir*.tiff" 
   
+}
+while true; do
+  clear
+	allscan48 raw/ links/
 done
