2ef5db5f8cbebf2a8233e6cb874f168154e6581b
dcfvg <qwsx@dcfvg.com>
2013-10-18 18:09:50 +0200

    Add pi subfolder in assets

--- server/install.sh
+++ server/install.sh
@@ -5,7 +5,7 @@ echo "creating assets ..."
 mkdir -v assets 
 
 cd assets
-mkdir -v 01-printbox/ 02-archives/ 03-replay/
+mkdir -v 01-printbox/ 02-archives/ 03-replay/ pi/
 
 cd 02-archives
 mkdir -v cam01/ cam02/ A3-trace/ A4-docs/
