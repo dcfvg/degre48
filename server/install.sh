7ae5e73289488ad84842cbdbe54c05f9f8dc58d7
dcfvg <qwsx@dcfvg.com>
2013-11-15 12:22:18 +0100

    first working version

--- server/install.sh
+++ server/install.sh
@@ -8,10 +8,12 @@ cd assets
 mkdir -v 01-printbox/ 03-replay/ 00-import
 
 cd 00-import
-mkdir -v phone01 phone02 table
+mkdir -v phone table
 
 cd ../  # back to assets
 cp -r 00-import 02-archives
+
+mkdir 02-archives/printedpdf
 
 cd ../  # back to root
 mkdir -v tex_template/links
