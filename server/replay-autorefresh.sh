6c2bf86796311e22fd376972815ce5babb1c17e5
dcfvg <qwsx@dcfvg.com>
2013-11-15 13:05:10 +0100

    fix fix

--- server/replay-autorefresh.sh
+++ server/replay-autorefresh.sh
@@ -4,8 +4,10 @@ cd "$(pwd)/assets/03-replay/"
 
 while true; do
 
-  ffmpeg -r 5 -y -f image2 -pattern_type glob -i '*.jpg' out.mp4
-  cp -rf ~/Desktop/live/live.mp4
+  #ffmpeg -r 5 -y -f image2 -pattern_type glob -i '*.jpg' out.mp4
+  ffmpeg -f image2 -pattern_type glob -i '*.jpg' -r 12 -vcodec mpeg4 -b 30000k -vf scale=1920:-1 -y tmp.mp4
+	
+  cp -rf tmp.mp4 ~/Desktop/live/live.mp4
   rm tmp.mp4
   
   for (( i=15; i>0; i--)); do
