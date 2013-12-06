39a6c78de55ae86c4e6ff62f9e8e74b748551dff
dcfvg <qwsx@dcfvg.com>
2013-12-06 11:32:25 +0100

    clean js

--- server/poster/get.js
+++ server/poster/get.js
@@ -3,20 +3,18 @@
 var page = new WebPage(),
     address, output, size;
  
-  address = "http://dev.d48/poster/";
-  width = 2970; height = 4150; // 4200
-  output = "assets/step.png";
- 
- 
-  page.viewportSize = { width: width };
+address = "http://dev.d48/poster/";
+width = 2970; height = 4150; // 4200
+output = "assets/step.png";
 
-  page.open(address, function (status) {
-      if (status !== 'success') {
-          console.log('Unable to load the address!');
-      } else {
-          window.setTimeout(function () {
-              page.render(output);
-              phantom.exit();
-          }, 900);
-      }
-  });
+
+page.viewportSize = { width: width, height:height};
+page.open(address, function (status) {
+  if (status !== 'success') { console.log('Unable to load the address!');
+  } else {
+    window.setTimeout(function () {
+        page.render(output);
+        phantom.exit();
+    }, 1000);
+  }
+});
