d0f4c2266cae69beee2422a954f14d7d3c8831b8
dcfvg <qwsx@dcfvg.com>
2013-12-06 14:30:05 +0100

    paper size fix

--- server/poster/get.js
+++ server/poster/get.js
@@ -6,7 +6,7 @@ var page = new WebPage(),
 // address = "http://dev.d48/poster/";
 
 address = "http://127.0.0.1/poster/";
-width = 2970; height = 4150; // 4200
+width = 2970; height = 4200; // 4200
 output = "assets/step.png";
 
 
